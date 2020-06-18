0a8762b962bc2574e9eb03f8a07037d7eaab68aa
Decska <66358871+Decska@users.noreply.github.com>
2020-06-18 08:32:41 +0200

    /*Events & Event Listeners, Excercise 2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 /*Events & Event Listeners, Excercise 1*/
 $('body').append('<p>He lights the lamp at 8.00pm</p>');
-$('button').on('click', () => {
+$('#first').on('click', () => {
     console.log ('Yeah, you clicked me!');
   })
+//Events & Event Listeners, Excercise 2
+$('#second').on('click', () => {
+  $('#first').text('Click ME!');
+    })
